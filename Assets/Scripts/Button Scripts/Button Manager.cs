637419c7e6f9b068fd137098b856c0f32d0b09d4
doia7002 <doia7002@naver.com>
2023-12-11 13:07:56 +0900

    Asset+

---
+++ Assets/Scripts/Button Scripts/Button Manager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void NewStart()
+    { 
+    
+    }
+    public void LoadStart() 
+    {
+    
+    }
+    public void Option()
+    {
+    
+    }
+    public void Exit()
+    {
+        
+    }
+}
